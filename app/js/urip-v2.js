cdb87239ccbb8c461c6d9a52de024cc541d95d86
kelvinatorr <kelvin.dealca@gmail.com>
2016-07-24 12:33:55 -0700

    refactor(urip): comment out form validations

--- app/js/urip-v2.js
+++ app/js/urip-v2.js
@@ -376,115 +376,115 @@
 			/* ==================================
 			 Contact Form Validation
 			 =====================================*/
-			$('#submit').click(function(e){
-
-				// Stop form submission & check the validation
-				e.preventDefault();
-
-				// Variable declaration
-				var error = false;
-				var fname = $('#fname').val();
-				var email = $('#email').val();
-				var subject = $('#subject').val();
-				var message = $('#message').val();
-
-				// Form field validation
-				if(fname.length == 0){
-					var error = true;
-					$('#fname').parent('div').addClass('field-error');
-				}else{
-					$('#fname').parent('div').removeClass('field-error');
-				}
-				if(email.length == 0 || email.indexOf('@') == '-1'){
-					var error = true;
-					$('#email').parent('div').addClass('field-error');
-				}else{
-					$('#email').parent('div').removeClass('field-error');
-				}
-				if(subject.length == 0){
-					var error = true;
-					$('#subject').parent('div').addClass('field-error');
-				}else{
-					$('#subject').parent('div').removeClass('field-error');
-				}
-				if(message.length == 0){
-					var error = true;
-					$('#message').parent('div').addClass('field-error');
-				}else{
-					$('#message').parent('div').removeClass('field-error');
-				}
-
-				if(error == true){
-					$('#error-notification').addClass('show-up');
-				}else{
-					$('#error-notification').removeClass('show-up');
-				}
-
-				if(error == false){
-					$.post("contact.php", $("#contact-form").serialize(),function(result){
-						if(result == 'sent'){
-							$('#success-notification').addClass('show-up');
-							$('.submit-btn').addClass('disabled');
-						}
-					});
-				}
-			});
+			//$('#submit').click(function(e){
+            //
+			//	// Stop form submission & check the validation
+			//	e.preventDefault();
+            //
+			//	// Variable declaration
+			//	var error = false;
+			//	var fname = $('#fname').val();
+			//	var email = $('#email').val();
+			//	var subject = $('#subject').val();
+			//	var message = $('#message').val();
+            //
+			//	// Form field validation
+			//	if(fname.length == 0){
+			//		var error = true;
+			//		$('#fname').parent('div').addClass('field-error');
+			//	}else{
+			//		$('#fname').parent('div').removeClass('field-error');
+			//	}
+			//	if(email.length == 0 || email.indexOf('@') == '-1'){
+			//		var error = true;
+			//		$('#email').parent('div').addClass('field-error');
+			//	}else{
+			//		$('#email').parent('div').removeClass('field-error');
+			//	}
+			//	if(subject.length == 0){
+			//		var error = true;
+			//		$('#subject').parent('div').addClass('field-error');
+			//	}else{
+			//		$('#subject').parent('div').removeClass('field-error');
+			//	}
+			//	if(message.length == 0){
+			//		var error = true;
+			//		$('#message').parent('div').addClass('field-error');
+			//	}else{
+			//		$('#message').parent('div').removeClass('field-error');
+			//	}
+            //
+			//	if(error == true){
+			//		$('#error-notification').addClass('show-up');
+			//	}else{
+			//		$('#error-notification').removeClass('show-up');
+			//	}
+            //
+			//	if(error == false){
+			//		$.post("contact.php", $("#contact-form").serialize(),function(result){
+			//			if(result == 'sent'){
+			//				$('#success-notification').addClass('show-up');
+			//				$('.submit-btn').addClass('disabled');
+			//			}
+			//		});
+			//	}
+			//});
 
 			/* ==================================
 			 Hero Form Validation
 			 =====================================*/
-			$('#hero-submit').click(function(e){
-
-				// Stop form submission & check the validation
-				e.preventDefault();
-
-				// Variable declaration
-				var error = false;
-				var fname = $('#hero-fname').val();
-				var email = $('#hero-email').val();
-				var username = $('#hero-username').val();
-
-				// Form field validation
-				if(fname.length == 0){
-					var error = true;
-					$('#hero-fname').parent('div').addClass('field-error');
-				}else{
-					$('#hero-fname').parent('div').removeClass('field-error');
-				}
-				if(email.length == 0 || email.indexOf('@') == '-1'){
-					var error = true;
-					$('#hero-email').parent('div').addClass('field-error');
-				}else{
-					$('#hero-email').parent('div').removeClass('field-error');
-				}
-				if(username.length == 0){
-					var error = true;
-					$('#hero-username').parent('div').addClass('field-error');
-				}else{
-					$('#hero-username').parent('div').removeClass('field-error');
-				}
-
-				if(error == true){
-					$('#hero-error-notification').addClass('show-up');
-				}else{
-					$('#hero-error-notification').removeClass('show-up');
-				}
-
-				if(error == false){
-					$.post("hero-form.php", $("#register-form").serialize(),function(result){
-						if(result == 'sent'){
-							$('#hero-success-notification').addClass('show-up');
-							$('#hero-submit').addClass('disabled');
-						}
-					});
-				}
-			});
+			//$('#hero-submit').click(function(e){
+            //
+			//	// Stop form submission & check the validation
+			//	e.preventDefault();
+            //
+			//	// Variable declaration
+			//	var error = false;
+			//	var fname = $('#hero-fname').val();
+			//	var email = $('#hero-email').val();
+			//	var username = $('#hero-username').val();
+            //
+			//	// Form field validation
+			//	if(fname.length == 0){
+			//		var error = true;
+			//		$('#hero-fname').parent('div').addClass('field-error');
+			//	}else{
+			//		$('#hero-fname').parent('div').removeClass('field-error');
+			//	}
+			//	if(email.length == 0 || email.indexOf('@') == '-1'){
+			//		var error = true;
+			//		$('#hero-email').parent('div').addClass('field-error');
+			//	}else{
+			//		$('#hero-email').parent('div').removeClass('field-error');
+			//	}
+			//	if(username.length == 0){
+			//		var error = true;
+			//		$('#hero-username').parent('div').addClass('field-error');
+			//	}else{
+			//		$('#hero-username').parent('div').removeClass('field-error');
+			//	}
+            //
+			//	if(error == true){
+			//		$('#hero-error-notification').addClass('show-up');
+			//	}else{
+			//		$('#hero-error-notification').removeClass('show-up');
+			//	}
+            //
+			//	if(error == false){
+			//		$.post("hero-form.php", $("#register-form").serialize(),function(result){
+			//			if(result == 'sent'){
+			//				$('#hero-success-notification').addClass('show-up');
+			//				$('#hero-submit').addClass('disabled');
+			//			}
+			//		});
+			//	}
+			//});
 
 
 			// Function to close the Notification
-			$('a.notification-close').click(function(){
-				$(this).parent('div').fadeOut(200);
-			});
+			//$('a.notification-close').click(function(){
+			//	$(this).parent('div').fadeOut(200);
+			//});
 
 
 			/* ==========================
