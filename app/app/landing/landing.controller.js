72bd482408743c270d628feca377c4a99776e093
kelvinatorr <kelvin.dealca@gmail.com>
2016-08-02 15:42:20 -0700

    fix(landing): going from rsvp page to landing breaks map
    create the map after the digest cycle is complete

--- app/app/landing/landing.controller.js
+++ app/app/landing/landing.controller.js
@@ -3,40 +3,41 @@
     angular.module('tiffanyAndKelvin')
         .controller('LandingCtrl', LandingCtrl);
 
-    function LandingCtrl($scope, GoogleMapInit) {
+    function LandingCtrl($scope, GoogleMapInit, $timeout) {
         var vm = this;
 
         vm.mapsApiFailed = false;
 
         $scope.$on('$viewContentLoaded', function() {
             window.tak.runUripV2();
-        });
+            GoogleMapInit.mapsInitialized.then(function() {
+                var directionsDisplay = new google.maps.DirectionsRenderer;
+                var directionsService = new google.maps.DirectionsService;
 
-        GoogleMapInit.mapsInitialized.then(function() {
-            var directionsDisplay = new google.maps.DirectionsRenderer;
-            var directionsService = new google.maps.DirectionsService;
+                // make the map after the digest cycle is complete
+                $timeout(function() {
+                    var mapDiv = document.getElementById('googleMap');
+                    var map = new google.maps.Map(mapDiv, {
+                        center: {lat: 36.115662, lng: -115.170364},
+                        zoom: 12,
+                        scrollwheel: false
+                    });
+                    directionsDisplay.setMap(map);
+                    directionsDisplay.setPanel(document.getElementById('right-panel'));
 
-            var mapDiv = document.getElementById('googleMap');
-            var map = new google.maps.Map(mapDiv, {
-                center: {lat: 36.115662, lng: -115.170364},
-                zoom: 12,
-                scrollwheel: false
-            });
-            directionsDisplay.setMap(map);
-            directionsDisplay.setPanel(document.getElementById('right-panel'));
-
-            directionsService.route({
-                origin: '3555 S Las Vegas Blvd, Las Vegas, NV 89109',
-                destination: '8310 S Rainbow Blvd #100, Las Vegas, NV 89139',
-                travelMode: 'DRIVING'
-            }, function(response, status) {
-                if (status === 'OK') {
-                    console.log('got directions');
-                    directionsDisplay.setDirections(response);
-                } else {
-                    // Show iframe instead
-                    $scope.$apply(vm.mapsApiFailed = true);
-                }
+                    directionsService.route({
+                        origin: '3555 S Las Vegas Blvd, Las Vegas, NV 89109',
+                        destination: '8310 S Rainbow Blvd #100, Las Vegas, NV 89139',
+                        travelMode: 'DRIVING'
+                    }, function(response, status) {
+                        if (status === 'OK') {
+                            directionsDisplay.setDirections(response);
+                        } else {
+                            // Show iframe instead
+                            $scope.$apply(vm.mapsApiFailed = true);
+                        }
+                    });
+                });
             });
         });
     }
